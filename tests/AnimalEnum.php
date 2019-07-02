48e560444a1fb302915fd5e6a3ebfbbed5c41055
Mark Hartmann <mark-hartmann@mstone.de>
2019-07-02 19:07:47 +0200

    added some tests

---
+++ tests/AnimalEnum.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hartmann\Enum\Tests;
+
+
+use Hartmann\Enum\Enum;
+
+/**
+ * Class AnimalEnum
+ *
+ * @package Hartmann\Enum\Tests
+ *
+ * @method static \AnimalEnum Dog()
+ * @method static \AnimalEnum Cat()
+ * @method static \AnimalEnum Horse()
+ * @method static \AnimalEnum Fish()
+ */
+class AnimalEnum extends Enum
+{
+    protected const Dog = 0;
+    protected const Cat = 2;
+    protected const Horse = 4;
+    protected const Fish = 'blubb';
+}
---
+++ tests/EnumTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Hartmann\Enum\Tests;
+
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class EnumTest extends TestCase
+{
+    public function testGetValues(): void
+    {
+        $this->assertEquals([0, 2, 4, 'blubb'], AnimalEnum::getValues());
+    }
+
+    public function testGetNames(): void
+    {
+        $this->assertEquals(['Dog', 'Cat', 'Horse', 'Fish'], AnimalEnum::getNames());
+    }
+
+    public function testToArray(): void
+    {
+        $this->assertEquals([
+            'Dog' => 0,
+            'Cat' => 2,
+            'Horse' => 4,
+            'Fish' => 'blubb',
+        ], AnimalEnum::toArray());
+    }
+
+    public function testIsDefined(): void
+    {
+        $this->assertTrue(AnimalEnum::isDefined(0));
+        $this->assertFalse(AnimalEnum::isDefined(1));
+        $this->assertTrue(AnimalEnum::isDefined(2));
+        $this->assertTrue(AnimalEnum::isDefined('blubb'));
+        $this->assertFalse(AnimalEnum::isDefined('meow'));
+    }
+
+    public function testInstantiatingWithWrongValueThrowsException(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new AnimalEnum('meow');
+        new AnimalEnum(new stdClass());
+    }
+
+    public function testGetValue(): void
+    {
+        $cat = AnimalEnum::Cat();
+        $horse = AnimalEnum::Horse();
+
+        $this->assertEquals(2, $cat->getValue());
+        $this->assertEquals(4, $horse->getValue());
+    }
+
+    public function testGetName(): void
+    {
+        $cat = AnimalEnum::Cat();
+        $horse = AnimalEnum::Horse();
+
+        $this->assertEquals('Cat', $cat->getName());
+        $this->assertEquals('Horse', $horse->getName());
+    }
+
+    public function testEquals(): void
+    {
+        $cat = AnimalEnum::Cat();
+        $horse = AnimalEnum::Horse();
+
+        $this->assertTrue($cat->equals(AnimalEnum::Cat()));
+        $this->assertFalse($cat->equals(AnimalEnum::Horse()));
+        $this->assertTrue($horse->equals(AnimalEnum::Horse()));
+        $this->assertFalse($horse->equals(AnimalEnum::Dog()));
+    }
+
+    public function testInstantiatingWorksIfOtherEnumIsPassed(): void
+    {
+        $animal = new AnimalEnum(AnimalEnum::Fish());
+
+        $this->assertEquals('blubb', $animal->getValue());
+        $this->assertEquals('Fish', $animal->getName());
+        $this->assertEquals($animal, AnimalEnum::Fish());
+        $this->assertNotEquals($animal, AnimalEnum::Cat());
+    }
+
+    public function testToString(): void
+    {
+        $this->assertEquals('0', (string)AnimalEnum::Dog());
+        $this->assertEquals('2', (string)AnimalEnum::Cat());
+        $this->assertEquals('4', (string)AnimalEnum::Horse());
+        $this->assertEquals('blubb', (string)AnimalEnum::Fish());
+    }
+}
